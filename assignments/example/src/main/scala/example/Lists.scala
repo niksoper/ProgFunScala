47177dc31b5456330ae717348481bc4886ee8aac
Nick Soper <nsoper@scottlogic.com>
2014-09-15 22:34:22 +0100

    Committing unsaved changes

--- assignments/example/src/main/scala/example/Lists.scala
+++ assignments/example/src/main/scala/example/Lists.scala
@@ -46,5 +46,17 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = 0
+  def max(xs: List[Int]): Int = {
+    
+    def pairmax(a: Int, b: Int) = {
+      if (a > b) {
+        a
+      } else{
+        b
+      }
+    }
+    
+    return pairmax(xs.head, )
+    
+  }
 }
